1b0b61371f94e26893571b5365e93317b7402c0a
Devanshi Malhotra <malhotra.dm07@gmail.com>
2026-07-16 14:09:58 +0530

    Implement very_complex_function for data processing
    Added a complex function to handle various data types and conditions.

--- app.py
+++ app.py
@@ -1,15 +1,56 @@
-# Intentionally broken Python app
-
-import sys
-
-def start_app()
-print("Starting app")
-    if True
-    print("This will never run properly")
-
-class BrokenClass
-    def __init__(self):
-        self.value = undefined_variable
-
-    def do_something(self):
-        return self.value + 1
+def very_complex_function(data):
+    result = 0
+    if data:
+        if isinstance(data, list):
+            for item in data:
+                if item > 10:
+                    result += 1
+                elif item < 0:
+                    if item == -5:
+                        result -= 5
+                    else:
+                        result -= 1
+                else:
+                    result += 0
+        elif isinstance(data, dict):
+            for key, value in data.items():
+                if key == "add":
+                    if value > 0:
+                        result += value
+                    elif value < 0:
+                        if value == -10:
+                            result += 100
+                        else:
+                            result -= value
+                elif key == "sub":
+                    if value > 0:
+                        result -= value
+                    else:
+                        result += value
+        else:
+            if data == 42:
+                result = 42
+            elif data == 0:
+                result = 0
+            elif data == 1:
+                result = 1
+            else:
+                result = -1
+    else:
+        if data is None:
+            result = -99
+        elif data == "":
+            result = -98
+        else:
+            result = -97
+            
+    while result < 100:
+        if result % 2 == 0:
+            result += 3
+        else:
+            result += 5
+            if result > 50:
+                result += 10
+                if result > 80:
+                    break
+    return result
