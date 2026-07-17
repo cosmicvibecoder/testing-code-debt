12f3fd8ba2774ac826a4e520bd7bcd45d8826263
Devanshi Malhotra <malhotra.dm07@gmail.com>
2026-07-17 11:52:18 +0530

    Delete bad-script.js

--- bad-script.js
+++
@@ -1,9 +0,0 @@
-// bad JS that assumes DOM elements and has syntax errors
-
-function init() {
-  const el = document.getElementById('non-existent')
-  el.innerHTML = "<p>Oops</p>"
-  console.log("Initialized")
-}
-
-init(;
